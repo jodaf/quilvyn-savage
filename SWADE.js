b5fe7f1b10be2b5ac63513772cfd410ada5bda2b
Jim Hayes <spinaltap12345@gmail.com>
2023-08-19 12:52:57 -0700

    jshint work.

--- SWADE.js
+++ SWADE.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 /*jshint esversion: 6 */
 /* jshint forin: false */
-/* globals ObjectViewer, Quilvyn, QuilvynRules, QuilvynUtils */
+/* globals Expr, ObjectViewer, Quilvyn, QuilvynRules, QuilvynUtils */
 "use strict";
 
 /*
